2bf39f5ddbd9652b2ef08b0779f197f14b76c2a3
itszan101 <itszan29@gmail.com>
2023-06-21 14:30:34 +0700

    remove array

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,6 +19,6 @@ $user = auth()->user();
     </x-slot>
 
     <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-        
+        <x-jet-welcome />
     </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Route::group([ "middleware" => ['auth:sanctum', config('jetstream.auth_session'), 'verified'] ], function() {
     Route::view('/dashboard', "dashboard")->name('dashboard');
-
+    
     Route::get('/user', [ UserController::class, "index_view" ])->name('user');
     Route::view('/user/new', "pages.user.user-new")->name('user.new');
     Route::view('/user/edit/{userId}', "pages.user.user-edit")->name('user.edit');
